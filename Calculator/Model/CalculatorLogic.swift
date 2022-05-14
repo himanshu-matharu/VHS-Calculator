50f06b7f29918a225624d59c6be9ad174a9d031b
Himanshu Matharu <89752238+himanshu-matharu@users.noreply.github.com>
2022-05-14 04:07:04 -0400

    Initial Commit

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,59 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Himanshu Matharu on 2022-04-07.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation : (n1:Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double){
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double?{
+        
+        if let n = number {
+            if symbol == "+/-" {
+                return n * -1
+            } else if symbol == "AC" {
+                return  0
+            } else if symbol == "%" {
+                return n * 0.01
+            }else if symbol == "=" {
+                return performTwoNumCalculation(n2:n)
+            }else{
+                self.intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumCalculation(n2:Double) -> Double?{
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            if operation == "+" {
+                return n1 + n2
+            }else if operation == "-" {
+                return n1 - n2
+            }else if operation == "×" {
+                return n1 * n2
+            }else if operation == "÷" {
+                return n1 / n2
+            }else{
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        
+        return nil
+        
+    }
+    
+}
